f128a911eae083c7ad2141b94bb1789d9a46458e
Juyi Wu <juyiwu@microsoft.com>
2024-07-15 16:51:51 +0800

    remove test class

--- src/test/java/com/aifound/ideagenerationservice/IdeagenerationserviceApplicationTests.java
+++ src/test/java/com/aifound/ideagenerationservice/IdeagenerationserviceApplicationTests.java
@@ -3,10 +3,10 @@ package com.aifound.ideagenerationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class IdeagenerationserviceApplicationTests {
 
-	@Test
+	// @ Test
 	void contextLoads() {
 	}
 
